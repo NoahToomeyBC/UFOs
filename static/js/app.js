3229b2c2f3a31a2892f81e7f812fff42515f276c
Nwtoomey <noahtoomeyBC@gmail.com>
2021-08-18 10:21:35 -0500

    Finished module, ready for challenge work

--- static/js/app.js
+++ static/js/app.js
@@ -1,30 +1,29 @@
 // Import the data from data.js
 const tableData = data;
-var tbody = d3.select("tbody");
+var tbody = d3.select('tbody');
 
-// First, clear out any existing data
 function buildTable(data) {
-    tbody.html("");
-
-   // Next, loop through each object in the data
-   // and append a row and cells for each value in the row
-    
+    // First, clear out any existing data
+    tbody.html('');
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
     data.forEach((dataRow) => {
-        let row = tbody.append("tr");
+      // Append a row to the table body
+      let row = tbody.append('tr');
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append('td');
+        cell.text(val);
+        }
+      );
+    });
+  }
 
-    // Loop through each field in the dataRow and add
-    // each value as a table cell (td)
 
-        Object.values(dataRow).forEach((val) => {
-          let cell = row.append("td");
-          cell.text(val);
-          }
-        );
-      });
-    
-}
-
-function handleClick() {
+  function handleClick() {
     // Grab the datetime value from the filter
     let date = d3.select("#datetime").property("value");
     let filteredData = tableData;
